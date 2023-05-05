8e4728e9a63684ff8ab1962a686680afde560509
toohandsome <hudcan9@gmail.com>
2023-05-05 14:10:15 +0800

    linux 端口转发

--- attach-agent/src/main/java/io/github/toohandsome/attach/core/LinuxPortForward.java
+++ attach-agent/src/main/java/io/github/toohandsome/attach/core/LinuxPortForward.java
@@ -1,18 +1,36 @@
 package io.github.toohandsome.attach.core;
 
+import io.github.toohandsome.attach.util.RuntimeUtil;
+
 /**
  * @author hudcan
  */
 public class LinuxPortForward implements PortForward {
 
+    String cmdStr = "iptables -t nat -I PREROUTING -p tcp --dport ? -j DNAT --to-destination ?:?";
+    String cmdStr1 = "echo 1 >/proc/sys/net/ipv4/ip_forward";
     @Override
     public void forward(int localPort, int remotePort) {
-
+        String format = String.format(cmdStr, localPort, "127.0.0.1", remotePort);
+        try {
+            Process exec = Runtime.getRuntime().exec(format);
+            int i = exec.waitFor();
+            RuntimeUtil.getOutStr(exec);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void forward(int localPort, String remoteAddr, int remotePort) {
-
+        String format = String.format(cmdStr, localPort, remoteAddr, remotePort);
+        try {
+            Process exec = Runtime.getRuntime().exec(format);
+            int i = exec.waitFor();
+            RuntimeUtil.getOutStr(exec);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
